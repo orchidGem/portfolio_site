83665f8b267fd6964ab4fa010f7e4a61a49d78ab
Laura Evans <lauraevans@Lauras-MacBook-Pro-2.local>
2016-09-17 18:17:50 +0300

    add contact modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 /*===== ALL ======== */
 
-// Change color of nav icon on scroll
-var icon = document.getElementsByClassName('glyphicon-envelope')[0];
+/* === Change color of nav icon on scroll === */
+var icon = document.getElementById('btn-contact-modal');
 var scrollHeight = (document.getElementsByTagName('nav')[0].clientHeight) / 2; // nav height / 2
 var changeNavIcon = function() {
   if (document.body.scrollTop > scrollHeight || document.documentElement.scrollTop > scrollHeight) {
@@ -12,8 +12,18 @@ var changeNavIcon = function() {
 };
 window.addEventListener("scroll", changeNavIcon);
 
+/* === toggle contact modal === */
+var contactModal = document.getElementById("contact-modal");
+var buttonCloseModal = document.getElementById("btn-close-modal");
+// icon variable declared in function above
+var toggleModal = function() {
+  contactModal.classList.toggle("showme");
+}; // end toggleModal function
 
-// fade in element on scroll
+icon.addEventListener("click", toggleModal);
+buttonCloseModal.addEventListener("click", toggleModal);
+
+/* === fade in element on scroll === */
 var fadeElements = document.getElementsByClassName("hideme");
 var fadeInElement = function() {
   for(i = 0; i < fadeElements.length; i++) {
